52101d057674753c7269ef1c2d4997742b9bd7bd
PrinceMatthew <princematthew@tutanota.com>
2021-03-29 17:42:42 +0300

    Add access token button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import LogoutButton from "./components/logout";
+import LoginButton from "./components/login";
 import {useAuth0} from "@auth0/auth0-react";
 import React from "react";
-import LoginButton from "./components/login";
 
 const App = () => {
-    const {user, isAuthenticated, isLoading} = useAuth0();
+    const {user, isAuthenticated, isLoading, getAccessTokenSilently} = useAuth0();
+
+    const [token, setToken] = React.useState("");
 
     if (isLoading) {
         return <div>Loading ...</div>;
     }
-    if (isAuthenticated === false)
+    if (isAuthenticated === false) {
         return <LoginButton/>
-    return (
-        isAuthenticated && (
-            <div>
-                <img src={user.picture} alt={user.name}/>
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-                <div><LogoutButton/></div>
-            </div>
-        )
-    );
-
+    }
+    else
+    {
+        const getAccessToken = async () => {
+            const token = await getAccessTokenSilently();
+            setToken(token);
+        };
+        return (
+            isAuthenticated && (
+                <div>
+                    <img src={user.picture} alt={user.name}/>
+                    <h2>{user.name}</h2>
+                    <p>{user.email}</p>
+                    <button onClick={getAccessToken}>Get token</button>
+                    <p>{token}</p>
+                    <div><LogoutButton/></div>
+                </div>
+            )
+        );
+    }
 };
 
 export default App;
+
+/*
+onClick={() =>
+        loginWithRedirect({
+          screen_hint: "signup",
+        })
+      }
+*/
