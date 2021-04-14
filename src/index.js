ddaed840be214d28e973ac782f3ef567a850c31b
PrinceMatthew <princematthew@tutanota.com>
2021-04-14 20:51:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//Create a Management API to get JWT tokens
+
 ReactDOM.render(
     <Auth0Provider
         domain="eseuri.eu.auth0.com"
