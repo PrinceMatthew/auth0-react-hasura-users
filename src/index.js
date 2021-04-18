a25f29ea73d75b91d71c10c76fc0ea0ead701e8c
PrinceMatthew <princematthew@tutanota.com>
2021-04-18 10:17:47 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 //Create a Management API to get JWT tokens
+//Set audience 
 
 ReactDOM.render(
     <Auth0Provider
         domain="eseuri.eu.auth0.com"
         clientId="2LvukLbgNsBz3l4lmvQkLNnpfDZkQEgC"
         redirectUri={window.location.origin}
+        audience="https://eseuri.eu.auth0.com/api/v2/"
     >
         <App />
     </Auth0Provider>,
